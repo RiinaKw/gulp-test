242a2b101c328e96f97c0668aa6e3eb3c9a0bd00
Riina K <riinak.tv@gmail.com>
2023-03-17 05:01:00 +0900

    エラーが起きたらデスクトップ通知

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,13 @@ const sass = require('gulp-sass')(require('sass'))
 const sourcemaps = require('gulp-sourcemaps')
 
 const debug = require('gulp-debug')
+const plumber = require("gulp-plumber")
+const notify = require("gulp-notify")
 
 exports.default = () => {
     return gulp.src('src/scss/**/*.{scss,css}')
+        // エラーが起きたときにデスクトップへ通知する
+        .pipe(plumber(notify.onError('Error: <%= error.message %>')))
         // ***.css.map の準備
         .pipe(sourcemaps.init())
         // コンパイル
