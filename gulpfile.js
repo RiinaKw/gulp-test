31efa71242abb64ce6e32088600aedc6ea96f7f7
Riina K <riinak.tv@gmail.com>
2023-03-17 04:52:10 +0900

    ***.css.map を出力

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,20 @@
 const gulp = require('gulp')
+
+// sass
 const sass = require('gulp-sass')(require('sass'))
+const sourcemaps = require('gulp-sourcemaps')
 
 exports.default = () => {
     return gulp.src('src/scss/**/*.{scss,css}')
+        // ***.css.map の準備
+        .pipe(sourcemaps.init())
+        // コンパイル
         .pipe(sass.sync({
             outputStyle: "expanded"
         }))
+        // ***.css.map を出力
+        .pipe(sourcemaps.write('./maps'))
+        // css を出力
         .pipe(gulp.dest('dist/css'))
 }
 
