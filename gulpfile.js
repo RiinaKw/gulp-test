bfd0afa25000b81442f1889ca32abf82fe53191a
Riina K <riinak.tv@gmail.com>
2023-03-17 05:10:45 +0900

    pug をコンパイル

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,10 @@ const gulp = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 const sourcemaps = require('gulp-sourcemaps')
 
+// pug
+const pug = require("gulp-pug")
+
+// その他
 const debug = require('gulp-debug')
 const plumber = require("gulp-plumber")
 const notify = require("gulp-notify")
@@ -26,4 +30,22 @@ const scssCompile = () => {
         .pipe(debug({ title: 'scss :' }))
 }
 
+/**
+ * pug をコンパイル
+ */
+const pugCompile = () => {
+    return gulp.src('src/pug/**/*.pug')
+        // エラーが起きたときにデスクトップへ通知する
+        .pipe(plumber(notify.onError('Error: <%= error.message %>')))
+        // pug コンパイル
+        .pipe(pug({
+            pretty: true
+        }))
+        // html を出力
+        .pipe(gulp.dest('dist'))
+        // ログ出力
+        .pipe(debug({ title: 'pug :' }))
+}
+
+exports.pug = pugCompile
 exports.scss = scssCompile
