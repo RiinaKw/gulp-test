b24181d7941981f586c2aad53d75fe37d90a348c
Riina K <riinak.tv@gmail.com>
2023-03-17 05:13:54 +0900

    引数なしで gulp を呼んだら pug と scss の両方が動く

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,26 @@ const debug = require('gulp-debug')
 const plumber = require("gulp-plumber")
 const notify = require("gulp-notify")
 
+/**
+ * pug をコンパイル
+ */
+const pugCompile = () => {
+    return gulp.src('src/pug/**/*.pug')
+        // エラーが起きたときにデスクトップへ通知する
+        .pipe(plumber(notify.onError('Error: <%= error.message %>')))
+        // pug コンパイル
+        .pipe(pug({
+            pretty: true
+        }))
+        // html を出力
+        .pipe(gulp.dest('dist'))
+        // ログ出力
+        .pipe(debug({ title: 'pug :' }))
+}
+
+/**
+ * scss をコンパイル
+ */
 const scssCompile = () => {
     return gulp.src('src/scss/**/*.{scss,css}')
         // エラーが起きたときにデスクトップへ通知する
@@ -30,22 +50,7 @@ const scssCompile = () => {
         .pipe(debug({ title: 'scss :' }))
 }
 
-/**
- * pug をコンパイル
- */
-const pugCompile = () => {
-    return gulp.src('src/pug/**/*.pug')
-        // エラーが起きたときにデスクトップへ通知する
-        .pipe(plumber(notify.onError('Error: <%= error.message %>')))
-        // pug コンパイル
-        .pipe(pug({
-            pretty: true
-        }))
-        // html を出力
-        .pipe(gulp.dest('dist'))
-        // ログ出力
-        .pipe(debug({ title: 'pug :' }))
-}
-
 exports.pug = pugCompile
 exports.scss = scssCompile
+
+exports.default = gulp.parallel(pugCompile, scssCompile)
