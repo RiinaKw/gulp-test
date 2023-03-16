32414649d210e0db0531b3e16cfc60c8eb1b6f8a
Riina K <riinak.tv@gmail.com>
2023-03-17 04:54:13 +0900

    ログを出力

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ const gulp = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 const sourcemaps = require('gulp-sourcemaps')
 
+const debug = require('gulp-debug')
+
 exports.default = () => {
     return gulp.src('src/scss/**/*.{scss,css}')
         // ***.css.map の準備
@@ -16,5 +18,7 @@ exports.default = () => {
         .pipe(sourcemaps.write('./maps'))
         // css を出力
         .pipe(gulp.dest('dist/css'))
+        // ログ出力
+        .pipe(debug({ title: 'scss :' }))
 }
 
